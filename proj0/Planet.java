a7180692a8e6674990f0ab72336134731e6473d8
Naveen Minhas <avuminhas05@gmail.com>
2019-10-11 19:10:42 +0530

    Planet.java:- implemented all methods

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -35,13 +35,72 @@ public class Planet{
 
 	/* Returns the force exerted by the argument Planet on 
 	* this Planet.
-	*/
+	**/
 	public double calcForceExertedBy(Planet p){
+		if(this == p) return 0;
 		double distance = this.calcDistance(p);
 		double m1 = this.mass;
 		double m2 = p.mass;
 		double rSquare = distance * distance;
 		return (gravConstant*m1*m2)/rSquare;
+	}
+
+	/* Returns the force exerted by the argument Planet on 
+	* this Planet in X direction.
+	**/
+	public double calcForceExertedByX(Planet p){
+		if(this == p) return 0;
+		double force = calcForceExertedBy(p);
+		double r = calcDistance(p);								
+		double dx = p.xxPos - this.xxPos;
+		return (force*dx)/r;
+	}
+
+	/* Returns the force exerted by the argument Planet on 
+	* this Planet in Y-direction.
+	**/
+	public double calcForceExertedByY(Planet p){
+		if(this == p) return 0;
+		double force = calcForceExertedBy(p);
+		double r = calcDistance(p);
+		double dy = p.yyPos - this.yyPos;
+		return (force*dy)/r;
+	}
+
+	/* Returns the net force exerted by the given array of 
+	* planets on this planet.
+	**/
+	public double calcNetForceExertedByX(Planet[] planets){
+		double netForceX = 0.0;
+		for(Planet p:planets){
+			netForceX += calcForceExertedByX(p);
+		}
+		return netForceX;
+	}
+
+	/* Returns the net force exerted by given array
+	* of palents on this planet.
+	**/
+	public double calcNetForceExertedByY(Planet[] planets){
+		double netForceY = 0.0;
+		for(Planet p: planets){
+			netForceY += calcForceExertedByY(p);
+		}
+		return netForceY;
+	}
+
+	/* Updates the velocity and position of this planet according to
+	* the given forces(fx, fy) in x,y direction and small change
+	* in time (dt).
+	**/
+	public void update(double dt, double fx, double fy){
+		double accX = fx/this.mass;
+		this.xxVel += accX*dt;
+		this.xxPos += xxVel*dt;
+
+		double accY = fy/this.mass;
+		this.yyVel += accY*dt;
+		this.yyPos += yyVel*dt;
 	}
 
 
